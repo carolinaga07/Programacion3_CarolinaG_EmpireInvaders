e386c32061f811e9600571911db2ab1546b3b364
carolina <carolina@MONNIE>
2026-08-06 13:59:56 -0400

    Proyecto inicial de empire invaders

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    public static void main(String[] args) throws Exception {
+       launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+       FXMLLoader loader = new FXMLLoader(getClass().getResource(STYLESHEET_CASPIAN));
+       Scene scene = new Scene(loader.load());
+       stage.setTitle("Empire Invaders");
+       stage.setScene(scene);
+       stage.show();
+
+    }
+}
