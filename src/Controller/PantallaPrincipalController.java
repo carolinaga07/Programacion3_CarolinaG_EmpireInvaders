ec43cba0d84fd895ee7d0c369e555f9e035a68c2
carolina <carolina@MONNIE>
2026-08-06 23:46:29 -0400

    creacion y edicion en clases navegacion y gamestate

--- src/Controller/PantallaPrincipalController.java
+++ src/Controller/PantallaPrincipalController.java
@@ -38,6 +38,7 @@ public class PantallaPrincipalController  {
         lblBoton.setCursor(Cursor.HAND);
 
         lblBoton.setOnMouseClicked(event ->{
+            
 
         });
     }
---
+++ src/Model/GameState.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class GameState {
+
+    private int puntaje;
+    private int vidas;
+    private int nivel;
+
+    public GameState(){
+        this.puntaje = 0;
+        this.vidas = 3;
+        this.nivel = 1;
+    }
+
+    
+
+
+
+   
+    
+    
+}
