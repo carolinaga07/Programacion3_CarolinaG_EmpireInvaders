5cd7e80745e6c7e71546224c8dcd6ed1d554fea5
carolina <carolina@MONNIE>
2026-08-08 15:36:24 -0400

    eliminacion deimagenes no necesarias

--- src/View/SpritesJuego.java
+++ src/View/SpritesJuego.java
@@ -13,7 +13,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+
 
 public class SpritesJuego {
     private Image fondo;
